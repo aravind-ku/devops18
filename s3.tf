c1c89bfdde7e08148c8ea547c5fab24fe23e3d37
Aravind <135971349+aravind-ku@users.noreply.github.com>
2026-07-28 23:23:42 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -25,7 +25,7 @@ status = "Enabled"
 terraform {
 backend "s3" {
 region = "ap-south-1"
-bucket = "aravind.sushmarani.k8s"
+bucket = "aravind.sushmaranidas.k8s"
 key = "prod/terraform.tfstate"
 }
 }
